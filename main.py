b6527ca751c9d7d5e234b26e270f3c5fffa13fd5
Dylan <dborc610@mtroyal.ca>
2023-03-03 17:44:36 -0700

    taken seats show up as empty

--- main.py
+++ main.py
@@ -6,21 +6,20 @@ import functools
 import re
 
 
-front_window_seat_weight = 1.5
-back_window_seat_weight = 1.7
-blinded_by_light_weight = 0.15
-front_better_window_weight = 0.3
-back_better_window_weight = 0.3
-middle_taken_beside_weight = -1.0
-closer_to_front = 0.008 # per seat
-asile_seat_weight = 1.3
-leg_room_weight = 0.05
-sec1_weight = 0.01 #sections 2-5
+front_window_seat_weight = 0.8
+back_window_seat_weight = 1
+blinded_by_light_weight = 0.1
+front_better_window_weight = 1
+back_better_window_weight = 1
+closer_to_front = 0.003 # per seat
+asile_seat_weight = 1
+leg_room_weight = 0
+sec1_weight = 0.1 #sections 2-5
 sec2_weight = 0.1 #sections 6-10
-sec3_weight = 0.1 #setion 11-12
-sec4_weight = 0.15 #sections 13-18
-sec5_weight = -0.8 #sections 19-23
-seat_empty_beside = 2.0
+sec3_weight = 0.05 #setion 11-12
+sec4_weight = 0.1 #sections 13-18
+sec5_weight = 1.5 #sections 19-23
+seat_empty_beside = 2.5
 
 def get_seat_satus(seatID, seatlist):
     for seat in seatlist:
@@ -29,20 +28,26 @@ def get_seat_satus(seatID, seatlist):
 
 def score_seat(seatID, status, seatlist):
     points = 0.0 # base points
+    if status != 0:
+        return 0.0
     seatLetter = re.sub(r'[0-9]', '', seatID)
     seatNumber = int(re.sub(r'[A-F]', '', seatID))
-    if seatNumber <= 10 and seatLetter == "A" or seatLetter == "F":
+    if seatNumber <= 10 and (seatLetter == "A" or seatLetter == "F"):
         points += front_window_seat_weight
-    if seatNumber >= 13 and seatLetter == "A" or seatLetter == "F":
+    if seatNumber >= 13 and (seatLetter == "A" or seatLetter == "F"):
         points += back_window_seat_weight
+
     if seatNumber <= 10 and (seatLetter == "A" or seatLetter == "F") and seatNumber % 2 == 0:
         points += front_better_window_weight
-    if seatNumber >= 13 and (seatLetter == "A" or seatLetter == "F") and seatNumber % 2 == 0:
+    if seatNumber >= 13 and (seatLetter == "A" or seatLetter == "F") and seatNumber % 2 == 1:
         points += back_better_window_weight
+
     if seatLetter == "A" or seatLetter == "C" or seatLetter == "F":
         points += blinded_by_light_weight
+
     if seatLetter == "C" or seatLetter == "D":
         points += asile_seat_weight
+
     if seatNumber == 1 or seatNumber == 11 or seatNumber == 12:
         points += leg_room_weight
     if seatNumber <= 5 and seatNumber > 1:
@@ -54,17 +59,16 @@ def score_seat(seatID, status, seatlist):
     if seatNumber <= 19 and seatNumber > 13:
         points += sec4_weight
     if seatNumber <= 23 and seatNumber > 19:
-        points += sec5_weight
-    if seatLetter == "A" and int(get_seat_satus('{seatNumber}B', seatlist)) > 0:
+        points -= sec5_weight
+
+    if (seatLetter == "A" or seatLetter == "C") and get_seat_satus(str(seatNumber)+"B", seatlist) == 0:
+        points += seat_empty_beside
+    if (seatLetter == "D" or seatLetter == "F") and get_seat_satus(str(seatNumber)+"E", seatlist) == 0:
         points += seat_empty_beside
 
-    
-    
-    
     row_multiplier = (23 - seatNumber ) * closer_to_front
     points += row_multiplier
-    if status != 0:
-        points = 0.0
+
     return points
 
 def compare(item1, item2):
@@ -80,19 +84,19 @@ def compare(item1, item2):
     
 def draw_plane(seatlist):
     for index in range(len(seatlist)):
-        row_num = re.sub(r'[A-F]', '', seatlist[index]["Seat"])
         if index < len(seatlist) - 5:
-            a = float(seatlist[index]["points"])
-            b = float(seatlist[index+1]["points"])
-            c = float(seatlist[index+2]["points"])
-            d = float(seatlist[index+3]["points"])
-            e = float(seatlist[index+4]["points"])
-            f = float(seatlist[index+5]["points"])
+            row_num = re.sub(r'[A-F]', '', seatlist[index]["Seat"])
+            a = "" if seatlist[index]["points"] == 0 else str('{0:>5.2f}'.format(float(seatlist[index]["points"])))
+            b = "" if seatlist[index+1]["points"] == 0 else str('{0:>5.2f}'.format(float(seatlist[index+1]["points"])))
+            c = "" if seatlist[index+2]["points"] == 0 else str('{0:>5.2f}'.format(float(seatlist[index+2]["points"])))
+            d = "" if seatlist[index+3]["points"] == 0 else str('{0:>5.2f}'.format(float(seatlist[index+3]["points"])))
+            e = "" if seatlist[index+4]["points"] == 0 else str('{0:>5.2f}'.format(float(seatlist[index+4]["points"])))
+            f = "" if seatlist[index+5]["points"] == 0 else str('{0:>5.2f}'.format(float(seatlist[index+5]["points"])))
         
             if re.sub(r'[0-9]', '', seatlist[index]["Seat"]) == "A":
-                print('{0:>2} [{1:>5.2f}] [{2:>5.2f}] [{3:>5.2f}]    [{4:>5.2f}] [{5:>5.2f}] [{6:>5.2f}]'.format(row_num, a, b, c, d, e, f))
+                print('{0:>2} [{1:>5}] [{2:>5}] [{3:>5}]    [{4:>5}] [{5:>5}] [{6:>5}]'.format(row_num, a, b, c, d, e, f))
             elif seatlist[index]["Seat"]== "11B":
-                print('{0:>2}         [{1:>5.2f}] [{2:>5.2f}]    [{3:>5.2f}] [{4:>5.2f}]      '.format(row_num, a, b, c, d))
+                print('{0:>2}         [{1:>5}] [{2:>5}]    [{3:>5}] [{4:>5}]      '.format(row_num, a, b, c, d))
             else:
                 continue
         
@@ -113,7 +117,7 @@ flights = [x for x in allBookings if x["xType"] == "Charter"]
 camps = [x for x in allBookings if x["xType"] == "Camp"]
 
 for index in range(1):
-    index = 9
+    index = 5
     FromLocation = flights[index]["xFromLocation"]
     ToLocation = flights[index]["xToLocation"]
     StartDate = flights[index]["xStartDate"]
